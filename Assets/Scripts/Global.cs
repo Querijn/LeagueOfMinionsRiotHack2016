dc845c2a950435889e27b3ae7568ec5bb7cfd731
Matviy <mikeads66@gmail.com>
2016-09-21 19:59:02 -0700

    New Matt's Scene and global (camera) script
    Added a new scene, and added a script that attaches to the camera and
    records the current active selection, and moves a sphere on top of it as
    a marker.

---
+++ Assets/Scripts/Global.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Global : MonoBehaviour {
+
+    //Record which game object is currently selected.
+    GameObject selected_object;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Detect a mousedown on a minion object.
+        //TODO: Filter to only work on minion gameObjects?
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100.0f) && hit.transform.gameObject.name == "Minion")
+            {
+                selected_object = hit.transform.gameObject;           
+            }
+        }
+
+        //Right-Click to deselect
+        if (Input.GetMouseButtonDown(1))
+        {
+            selected_object = null;
+            GameObject.Find("Sphere").transform.position = new Vector3(500, 500, 500); //Offscreen
+        }
+
+        //Draw a selection indicator (red dot?) above the game object.
+        if(selected_object != null)
+        {
+            Vector3 position = selected_object.transform.position;
+            position.y += 1; //Move up above minion.
+            GameObject.Find("Sphere").transform.position = position;
+        }
+
+	}
+}
