e8dd3786cb7ab86717d7688dff20be88b2a80339
honux <atoahp@hotmail.com>
2016-09-23 11:49:11 -0700

    Added "The 1 health nexus throw"

--- Assets/Scripts/RedTeamAI.cs
+++ Assets/Scripts/RedTeamAI.cs
@@ -7,6 +7,8 @@ public class RedTeamAI : MonoBehaviour {
     protected float m_nextSpawn;
     public float m_maximumMinions;
 
+    protected bool hasUpdatedRate = false;
+    protected float defaultUpdateRate = 0.2f;
 	// Use this for initialization
 	void Start ()
     {
@@ -18,6 +20,26 @@ public class RedTeamAI : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        if (!hasUpdatedRate)
+        {
+            float turrentCount = 0;
+            foreach (GameObject tw in GameObject.FindGameObjectsWithTag("Tower"))
+            {
+                Team t_MyTeam = tw.transform.parent.GetComponent<Team>();
+                if (t_MyTeam && t_MyTeam.m_Team == Team.TeamType.Red)
+                    turrentCount++;
+            }
+
+            if (turrentCount <= 1)
+            {
+                defaultUpdateRate = 0.15f;
+                m_maximumMinions += 3;
+                hasUpdatedRate = true;
+            }
+        }
+
+
+
 	    if (Time.time >= m_nextSpawn && Global.redMinionsCount < m_maximumMinions)
         {
             SpawnMinion();
