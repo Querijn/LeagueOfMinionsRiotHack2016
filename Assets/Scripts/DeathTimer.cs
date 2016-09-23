c789ab9a7bca52d088350076764a615740133b27
honux <atoahp@hotmail.com>
2016-09-23 05:47:32 -0700

    Added the bombing minion

---
+++ Assets/Scripts/DeathTimer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class DeathTimer : MonoBehaviour
+{
+    public float m_countdown;
+    public float m_bombRadius;
+    protected float m_deathTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_deathTime = Time.time + m_countdown;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TextMesh text = gameObject.GetComponent<TextMesh>();
+        if (text)
+            text.text = ((int)(m_deathTime - Time.time)).ToString();
+
+        if (Time.time > m_deathTime && transform.parent)
+        {
+            Collider[] hitColliders = Physics.OverlapSphere(gameObject.transform.position, m_bombRadius);
+            foreach (Collider thing in hitColliders)
+            {
+                if (thing.tag == "Minion")
+                    thing.SendMessage("Die");
+            }
+            Destroy(gameObject);
+        }
+    }
+}
