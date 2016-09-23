e9539b5f7e73dbfff93a91c8c8c9ce3802f222fb
honux <atoahp@hotmail.com>
2016-09-22 19:24:18 -0700

    Added Minion Spawner, updated the Working BG
    - Added Minion Spawner + Minion Spawner Prefab;
    - UI now gets the total minions from the spawner maximum minions;
    - Updated the Working BG to include a minion spawner.

---
+++ Assets/MinionSpawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinionSpawner : MonoBehaviour {
+
+    public float totalMinions;
+    public float spawnRate;
+    public float minionsLeft;
+    public GameObject minionClass;
+
+    void SpawnMinion()
+    {
+        Instantiate(minionClass, transform.position, Quaternion.identity);
+
+        minionsLeft -= 1;
+        if (minionsLeft <= 0)
+        {
+            CancelInvoke("SpawnMinion");
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        minionsLeft = totalMinions;
+        InvokeRepeating("SpawnMinion", 5, spawnRate);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
